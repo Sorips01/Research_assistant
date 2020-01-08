83892bcac602823b94f71ed8c83d1eabc1a63600
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-01-08 23:12:53 +0900

    BER 측정 (추가)
    추가
    1. 코드 추가
    BER 측정 부분 추가

--- PSK_BER_measure.m
+++ PSK_BER_measure.m
@@ -1,6 +1,10 @@
 
 % (1) Binary-PSK modulation
+disp(' start');
 message=randi([0,1],1,10);                    % 랜덤하게 10개 Binary 숫자 생성
+disp(' message : ');
+disp(message);
+
 bit_period=.000001;
 A=5;                                          % Amplitude of carrier signal 
 bit_rate=1/bit_period;                        % bit rate
@@ -39,13 +43,13 @@ title('waveform for binary PSK modulation and noise');
 
 % (4) Binary PSK demodulation
 PSK_demodulation=[];
-for n=ss:ss:length(message)
-  t=bit_period/99:bit_period/99:bit_period;
-  y=cos(2*pi*f*t);                                        % carrier signal 
-  mm=y.*message((n-(ss-1)):n); %??
-  integration = trapz(t,mm);                              % 사다리꼴 적분 
+for n=ss:ss:length(PSK_noise)
+  t3=bit_period/99:bit_period/99:bit_period;
+  y=cos(2*pi*f*t3);                                        % carrier signal 
+  section=y.*PSK_noise((n-(ss-1)):n); % n번째 구간 지정
+  integration = trapz(t3,section);                         % 사다리꼴 적분 
   value=round((2*integration/bit_period));                                     
-  if(value>0)                                        % logic level = (A+A)/2=0 
+  if(value>0)                                     % logic level = (A+A)/2=0 
                          %becouse A*cos(2*pi*f*t+pi) means -A*cos(2*pi*f*t)
     a=1;
   else
@@ -56,5 +60,15 @@ end
 disp(' Binary information at Reciver :');
 disp(PSK_demodulation);
 
+% (5) BER 측정
+count=0;
+for i=1:1:length(message)
+    if(message(1,i) ~= PSK_demodulation(1,i)) % 복조한 값이 다르면 에러 발생
+        count=count+1;
+    end
+end
+BER=count/length(message);
+disp(' BER :');
+disp(BER);
 
 
