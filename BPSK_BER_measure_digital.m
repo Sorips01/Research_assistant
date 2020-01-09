05e8aa8b59b3821e976919970f70a48f5c081d89
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-01-09 19:02:26 +0900

    복조 (추가)
    추가
    1. 코드 추가
    심벌화 후 복조하는 코드를 추가함.

--- BPSK_BER_measure_digital.m
+++ BPSK_BER_measure_digital.m
@@ -7,8 +7,15 @@ for i = 1:1:length(message)
     end
 end
 
-for x= 0:1:10
+for x_dB= 0:1:10
     epoch=0;
     error=0;
     while error==200
-        S=sum(PSK.^2)/length(PSK);
+        S=sum(simbol.^2)/length(simbol);
+        N=S*10^(-0.1*x_dB);
+        noise =sqrt(N/2)*randn(1,length(simbol));      % 잡음 생성
+        simbol_noise=simbol+noise;
+        simbol_demo = simbol_noise;
+        for j=1:1:length(message)
+            if(simbol_noise(1,j)>=0)
+                simbol_demo = 
