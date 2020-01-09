27320d342933536f1d9fd7b3b699cbc16234a1a0
kijiyeon <rlwldus305@naver.com>
2020-01-09 19:03:23 +0900

    BPSK_BER_measure_digital
    1. 추가
    코드 추가(for문, while문)

--- BPSK_BER_measure_digital.m
+++ BPSK_BER_measure_digital.m
@@ -4,15 +4,28 @@ close all;
 
 
 message = randn([0,1],1,100000);  %전송하는 메시지
-for x = 0:1:10
-   epoch = 0; % trial 횟수 
-   while(error = 200)
-       symbol+noise; %수신 심벌
-       
 for i = 1:1:length(message)
     if messege[i] == 1
         bit = 1;
     else
         bit = 0;
         
-        
+    end  
+end
+
+for x = 0:1:10
+   error = 0;
+   epoch = 0; % trial 횟수
+   
+   while(error == 200)
+      noise_xdB = sqrt(N/2)*randn(1,n);           % white_gaussian_noise(mean = 0, var = N_0dB)
+      receive = symbol + noise_xdB; %수신 심벌 
+        if(receive > 0)
+            receive_bit = 1;
+        else 
+            receive_bit = 0;
+        end
+   end
+   BER = error/(epoch*length(message));  %BER = 최종결과
+end
+
