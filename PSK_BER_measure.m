e5ec857ce0d749d422ded58fa49e37b75bc9576c
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-01-09 14:14:43 +0900

    코드 수정 (수정)
    수정
    1. 코드 수정
    message의 개수를 100개로 늘림

--- PSK_BER_measure.m
+++ PSK_BER_measure.m
@@ -1,7 +1,7 @@
 
 % (1) Binary-PSK modulation
 disp(' start');
-message=randi([0,1],1,10);                    % 랜덤하게 10개 Binary 숫자 생성
+message=randi([0,1],1,100);                    % 랜덤하게 10개 Binary 숫자 생성
 disp(' message : ');
 disp(message);
 
@@ -9,7 +9,7 @@ bit_period=.000001;
 A=5;                                          % Amplitude of carrier signal 
 bit_rate=1/bit_period;                        % bit rate
 f=bit_rate*2;                                 % carrier frequency(왜 x2?)                               
-t1=bit_period/99:bit_period/99:bit_period;    % 그래프를 그리기 위해 시간 간격 설정             
+t1=bit_period/1000:bit_period/1000:bit_period;    % 그래프를 그리기 위해 시간 간격 설정             
 ss=length(t1); 
 
 PSK=[];
@@ -22,7 +22,7 @@ for i=1:1:length(message)
     PSK=[PSK y];                 
 end
 
-t2=bit_period/99:bit_period/99:bit_period*length(message);
+t2=bit_period/1000:bit_period/1000:bit_period*length(message);
 subplot(3,1,1);
 plot(t2,PSK);
 xlabel('time(sec)');
@@ -35,7 +35,7 @@ title('waveform for binary PSK modulation coresponding binary information');
 % noise_40dB = 0 + sqrt(N_40dB)*randn(1,n);         % white_gaussian_noise(mean = 0, var = N_40dB)
 
 % (3) A combination of noise and modulation
-noise =0.2*rand(1,990);      % 잡음 생성
+noise =0.2*rand(1,100000);      % 잡음 생성
 PSK_noise = PSK + noise;     % PSK 변조에 잡음 추가
 subplot(3,1,2);
 plot(t2,PSK_noise);
@@ -44,7 +44,7 @@ title('waveform for binary PSK modulation and noise');
 % (4) Binary PSK demodulation
 PSK_demodulation=[];
 for n=ss:ss:length(PSK_noise)
-  t3=bit_period/99:bit_period/99:bit_period;
+  t3=bit_period/1000:bit_period/1000:bit_period;
   y=cos(2*pi*f*t3);                                        % carrier signal 
   section=y.*PSK_noise((n-(ss-1)):n); % n번째 구간 지정
   integration = trapz(t3,section);                         % 사다리꼴 적분 
