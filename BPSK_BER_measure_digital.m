7a8f554e1a3fbf7637ce552d46f154eb5f952a1e
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-01-11 00:12:59 +0900

    BER 계산 (추가)
    추가
    1. 코드 추가
    BER 계산하는 코드를 추가함

--- BPSK_BER_measure_digital.m
+++ BPSK_BER_measure_digital.m
@@ -1,5 +1,6 @@
 message=randi([0,1],1,1000000);
 count=0;
+BER=[];
 simbol=message;               %심볼화
 for i = 1:1:length(message)
     if(message(1,i)==0)
@@ -10,12 +11,14 @@ end
 for x_dB= 0:1:10
     epoch=0;
     error=0;
+    
     while error==200
         S=sum(simbol.^2)/length(simbol);
         N=S*10^(-0.1*x_dB);
         noise =sqrt(N/2)*randn(1,length(simbol));      % 잡음 생성
         simbol_noise=simbol+noise;
         simbol_demo = simbol_noise;
+        
         for j=1:1:length(message)                   %수신 심벌 복조
             if(simbol_noise(1,j)>=0)
                 simbol_demo(1,j) = 1;
@@ -31,6 +34,12 @@ for x_dB= 0:1:10
             end
         end
         
-        
-        
+        error_bit=message-simbolbit;
+        error=nnz(error_bit);       %error_bit 행렬에서 0이 아닌 원소의 개수를 센다
+        epoch = epoch+1;
+    end
+    
+    BER= [BER error/(epoch*length(message))];
+end
+
         
