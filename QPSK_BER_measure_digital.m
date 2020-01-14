400b6c2d2b733e016174f39e61ba27ce01c2b495
kijiyeon <rlwldus305@naver.com>
2020-01-15 01:04:55 +0900

    QPSK_BER_measure_digital(수정)
    1. 수정
    수정필요 _ 복조과정 방법 잘못됨
    수정 _ 코드수정

--- QPSK_BER_measure_digital.m
+++ QPSK_BER_measure_digital.m
@@ -7,19 +7,19 @@ message=randi([0,1],1,1000000);
 BER=[];
 symbol=message;               %심볼화
 
-for i = 1:2:(length(message)-2)
-    if(message(i) == 0 && message(i+1) == 0)
-        symbol(i) =-1;
-        symbol(i+1) = -1*j;
-    elseif(message(i) == 0 && message(i+1) == 1)
-        symbol(i) = -1;
-        symbol(i+1) = 1*j;
-    elseif(message(i) == 1 && message(i+1) == 0)
-        symbol(i) = 1;
-        symbol(i+1) = -1*j;
-    elseif(message(i) == 1 && message(i+1) == 1)
-        symbol(i) = 1;
-        symbol(i+1) = 1*j;
+for j = 1:2:(length(message)-2)
+    if(message(j) == 0 && message(j+1) == 0)
+        symbol(j) =-1;
+        symbol(j+1) = -1i;
+    elseif(message(j) == 0 && message(j+1) == 1)
+        symbol(j) = -1;
+        symbol(j+1) = 1i;
+    elseif(message(j) == 1 && message(j+1) == 0)
+        symbol(j) = 1;
+        symbol(j+1) = -1i;
+    elseif(message(j) == 1 && message(j+1) == 1)
+        symbol(j) = 1;
+        symbol(j+1) = 1i;
     end
 end
 
@@ -32,56 +32,50 @@ for x_dB= 0:1:10
     N=S*10^(-0.1*x_dB);
     
     while error_count<=200
-        noise =sqrt(N/2)*randn(1,length(symbol)) + i*sqrt(N/2)*randn(1,length(symbol));      % 잡음 생성
+        noise =sqrt(N/2)*randn(1,length(symbol)) + 1i*sqrt(N/2)*randn(1,length(symbol));      % 잡음 생성
         symbol_noise=symbol+noise;
         symbol_demo = zeros(1,length(symbol_noise));
         
-        for j=1:1:length(message)-2                   %수신 심벌 복조
-            for k = 2:2:length(message)
-            if(symbol_noise(1,j) <0 && symbol_noise(1,k) <0)
-                symbol_demo(j) = -1;
-                symbol_demo(k) = -1*i;
-            elseif(symbol_noise(1,j)<0 && symbol_noise(1,k) >0)
-                symbol_demo(j) = -1;
-                symbol_demo(k) = 1*i;
-            elseif(symbol_noise(1,j)>0 && symbol_noise(1,k)<0)
-                symbol_demo(j) = 1;
-                symbol_demo(k) = -1*i;
-            elseif(symbol_noise(1,j)>0 && symbol_noise(1,k) >0)
-                symbol_demo(j) = 1;
-                symbol_demo(k) = 1*i;
-
-            end
-         
-        end
+        for j=1:2:length(message)                   %수신 심벌 복조
+           if(symbol_noise(j)<0 && symbol_noise(j+1)<0)
+               symbol_demo(j) = -1;
+               symbol_demo(j+1) = -1i;
+           elseif(symbol_noise(j)<0 && symbol_noise(j+1)>0)
+               symbol_demo(j) = -1;
+               symbol_demo(j+1) = 1i;
+           elseif(symbol_noise(j)>0 && symbol_noise(j+1)<0)
+               symbol_demo(j) = 1;
+              symbol_demo(j+1) = -1i;
+           elseif(symbol_noise(j)>0 && symbol_noise(j+1)>0)
+               symbol_demo(j) = 1;
+               symbol_demo(j+1) = 1i;
+           end
+           
         end
         
         symbol_bit=zeros(1,length(symbol_noise));                   %복조한 심벌 비트로
         for j=1:2:(length(message)-2)
-            for k = 2:2:length(message)
-            if(symbol_demo(1,j)==-1 && symbol_noise(1,k) ==-1*i)
-                symbol_demo(j) = 0;
-                symbol_demo(k) = 0;
-            elseif(symbol_demo(1,j)==-1 && symbol_noise(1,k) ==1*i)
-                symbol_demo(j) = 0;
-                symbol_demo(k) = 1;
-            elseif(symbol_demo(1,j)==1 && symbol_noise(1,k) ==-1*i)
-                symbol_demo(j) = 1;
-                symbol_demo(k) = 0;
-            elseif(symbol_demo(1,j)==1 && symbol_noise(1,k) ==1*i)
-                symbol_demo(j) = 1;
-                symbol_demo(k) = 1;
-      
-                end
-            end
+           if(symbol_demo(j) == -1 &&symbol_demo(j+1)==-1i)
+               symbol_bit(j) = 0;
+               symbol_bit(j+1) = 0;
+           elseif(symbol_demo(j) == -1 &&symbol_demo(j+1)==1i)
+               symbol_bit(j) = 0;
+               symbol_bit(j+1) = 1;
+           elseif(symbol_demo(j) == 1 &&symbol_demo(j+1)==-1i)
+               symbol_bit(j) = 1;
+               symbol_bit(j+1) = 0;
+           elseif(symbol_demo(j) == 1 &&symbol_demo(j+1)==1i)
+               symbol_bit(j) = 1;
+               symbol_bit(j+1) = 1;
+           end
         end
         
         error_bit=message-symbol_bit;
         error=nnz(error_bit)       %error_bit 행렬에서 0이 아닌 원소의 개수를 센다
         error_count=error_count+error;
-        epoch = epoch+1;
+        
     end
-    
+    epoch = epoch+1;
     BER= [BER error_count/(epoch*length(message))]
 end
 
