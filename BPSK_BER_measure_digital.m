acb1378b31f26c0cbf3e7937bee3e5d8529f3edd
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-01-14 19:23:46 +0900

    최종 BPSK (수정)
    수정
    1. 코드 수정
    코드 수정함

--- BPSK_BER_measure_digital.m
+++ BPSK_BER_measure_digital.m
@@ -4,45 +4,48 @@ clear all;
 message=randi([0,1],1,1000000);
 count=0;
 BER=[];
-simbol=message;               %심볼화
+symbol=message;               %심볼화
 for i = 1:1:length(message)
     if(message(1,i)==0)
-        simbol(1,i)=-1;
+        symbol(1,i)=-1;
     end
 end
 
 for x_dB= 0:1:10
     epoch=0;
     error=0;
+    error_count=0;
     
-    while error<=200
-        S=(sum(simbol.^2))/length(simbol);
+    while error_count<=200
+        S=(sum(symbol.^2))/length(symbol);
         N=S*10^(-0.1*x_dB);
-        noise =sqrt(N/2)*randn(1,length(simbol));      % 잡음 생성
-        simbol_noise=simbol+noise;
-        simbol_demo = simbol_noise;
+        noise =sqrt(N/2)*randn(1,length(symbol));      % 잡음 생성
+        symbol_noise=symbol+noise;
+        symbol_demo = symbol_noise;
         
         for j=1:1:length(message)                   %수신 심벌 복조
-            if(simbol_noise(1,j)>=0)
-                simbol_demo(1,j) = 1;
+            if(symbol_noise(1,j)>=0)
+                symbol_demo(1,j) = 1;
             else
-                simbol_demo(1,j)= -1;
+                symbol_demo(1,j)= -1;
             end
         end
         
-        simbol_bit=simbol_demo;                   %복조한 심벌 비트로
+        symbol_bit=symbol_demo;                   %복조한 심벌 비트로
         for j=1:1:length(message)
-            if(simbol_demo(1,j)==-1)
-                simbol_bit(1,j)=0;
+            if(symbol_demo(1,j)==-1)
+                symbol_bit(1,j)=0;
             end
         end
         
-        error_bit=message-simbol_bit;
+        error_bit=message-symbol_bit;
         error=nnz(error_bit);       %error_bit 행렬에서 0이 아닌 원소의 개수를 센다
+        error_count=error_count+error;
         epoch = epoch+1;
     end
     
-    BER= [BER error/(epoch*length(message))];
+    BER= [BER error_count/(epoch*length(message))];
 end
+
 
         
