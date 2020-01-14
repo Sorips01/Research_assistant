84ef84392b8f3a918fa98e2814be45f5e4d68e32
kijiyeon <rlwldus305@naver.com>
2020-01-14 18:09:43 +0900

    BPSK_BER_measure_digital(수정)
    1. 수정
    코드 수정 (for문 순서, 변수명, 변수위치)

--- BPSK_BER_measure_digital.m
+++ BPSK_BER_measure_digital.m
@@ -3,24 +3,30 @@ clear all;
 close all;
 
 message = randi([0,1],1,100000);  %전송하는 메시지
+symbol = message;
 
 for i = 1:1:length(message)
-    if message[i] == 1
+    if message(i) == 1
         symbol(i) = 1;
     else
-        symbol(i) = 0;
+        symbol(i) = -1;
         
     end  
 end
 
-epoch = 0; % trial 횟수
-error = 0;
-for x = 0:1:10
-    
+
+for x = 1:1:10
+   error = 0;
+   epoch = 0; % trial 횟수
+   S = 1;
+   N = S*10.^(-0.1*x);
+   n = length(message);
    
-   while(error == 200)
-      noise_xdB = sqrt(N/2)*randn(1,n);           % white_gaussian_noise(mean = 0, var = N_0dB)
-      receive = symbol + noise_xdB; %수신 심벌 
+   while(error < 200)
+      noise = sqrt(N/2)*randn(1,n);           % white_gaussian_noise(mean = 0, var = N_0dB)
+      receive = symbol + noise; %수신 심벌
+      receive_bit = receive; 
+      
       for i = 1:1:length(message) 
           if(receive(i) > 0)
              receive_bit(i) = 1;
@@ -28,13 +34,15 @@ for x = 0:1:10
              receive_bit(i) = 0;
           end
       end
+      for i = 1:1:length(message)
+       if(message(i) - receive_bit(i) ~=0)
+           error = error +1;
+      end
       epoch  = epoch+1;
    end
-   for i = 1:1:length(message)
-       if(message(i) ~= receive_bit(i))
-           error ++;
-       end
+   BER = error/(epoch*length(message))  %BER = 최종결과
+   
    end
-  
+   
 end
- BER = error/(epoch*length(message))  %BER = 최종결과
+
