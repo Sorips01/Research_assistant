5dcaa0c9e0ba641d96c2c13dbdb8456333edba2d
fora22 <fora22@naver.com>
2020-01-28 20:02:56 +0900

    기능: 복조 기능 추가 완료(에러 발생)
    복조 기능 추가 완료
    1 epoch에 44만개 이상에 error 발생하여 수정 필요

--- QAM_16_measure_digital.m
+++ QAM_16_measure_digital.m
@@ -32,16 +32,27 @@ for x_dB= 0:1:10
    error_count=0;
   
     while (error_count<=200)
-        S=(sum(QAM_16_symbol.^2)/16);
+        %S = (sum(QAM_16_symbol.^2)/16);
+        S=1;
         N=S*10^(-0.1*x_dB);
         noise =sqrt(N/2)*randn(1,length(QAM_16_symbol)) + 1i*(sqrt(N/2)*randn(1,length(QAM_16_symbol)));      % 잡음 생성
         QAM_16_symbol_noise = QAM_16_symbol + noise;        %noise 추가
         
+        QAM_16_symbol_demo = zeros(1, length(QAM_16_symbol_noise));
+        
+        % Deomodulation Symbol
+        for i=1:1:length(QAM_16_symbol_noise)
+            QAM_16_symbol_demo(1, i) = distance_measure(QAM_16_symbol_noise(1, i));
+        end
+        
+        % Demodulation Symbol to bit
+        QAM_16_bit_demo = demo_symbol_to_bit(QAM_16_symbol_demo);
+        
         % error count, nnz, epoch plus 1
-        error_bit=message-bit_demo;
-        error=nnz(error_bit);       %error_bit 행렬에서 0이 아닌 원소의 개수를 센다 
-        error_count = error_count+error;
-        epoch = epoch+1;
+        error_bit = message - QAM_16_bit_demo;
+        error = nnz(error_bit);       %error_bit 행렬에서 0이 아닌 원소의 개수를 센다 
+        error_count = error_count + error;
+        epoch = epoch + 1;
         
     end
      QAM_16 = [QAM_16 error_count/(epoch*length(message))];
@@ -77,9 +88,10 @@ end
 comp_number = (comp(1) * comp(2)) + (comp(3) * comp(4) * 1i);
 end
 
-% 거리 구하기
+% 거리 구하기, 심벌 복조용
 function symbol_demo = distance_measure(symbol)
 point = [-3+3i -3+1i -3-1i -3-3i -1+3i -1+1i -1-1i -1-3i 1+3i 1+1i 1-1i 1-3i 3+3i 3+1i 3-1i 3-3i];
+point = point ./ sqrt(10);
 
 symbol_real = real(symbol);
 symbol_imag = imag(symbol);
@@ -87,10 +99,46 @@ symbol_imag = imag(symbol);
 point_to_point = zeros(1, length(point));
 
 for i=1:1:length(point)
-    point_to_point(1, i) = sqrt((symbol_real - real(point(1, i)))^2 + (symbol_imag - imag(point(1, i)))^2);
+    point_to_point(1, i) = sqrt((symbol_real - real(point(1, i)))^2 + (symbol_imag - imag(point(1, i)))^2); %sqrt((symbol_real - real(point(1, i))).^2 + (symbol_imag - imag(point(1, i))).^2);
 end
 
 distance = min(point_to_point);
 [x, y] = find(point_to_point == distance);
 symbol_demo = point(1, y);
+end
+
+% 심벌을 비트로 바꾸는 함수
+function bit_demo = demo_symbol_to_bit(symbol)
+symbol_real = real(symbol);
+symbol_imag = imag(symbol);
+
+bit_demo = zeros(1, length(symbol)*4);
+k = 1;
+for i=1:4:length(symbol)
+    if symbol_real(1, k) < 0
+        bit_demo(1, i) = 0;
+    else
+        bit_demo(1, i) = 1;
+    end
+
+    if abs(symbol_real(1, k)) > 2
+        bit_demo(1, i + 1) = 0;
+    else
+        bit_demo(1, i + 1) = 1;
+    end
+
+    if symbol_imag(1, k) > 0
+        bit_demo(1, i + 2) = 0;
+    else
+        bit_demo(1, i + 2) = 1;
+    end
+
+    if abs(symbol_imag(1, k)) > 2
+        bit_demo(1, i + 3) = 0;
+    else
+        bit_demo(1, i + 3) = 1;
+    end
+    k = k + 1;
+end
+
 end
