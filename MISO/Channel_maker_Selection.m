14024e7c3973709078cf9365aed5e0092179a974
kiJiyeon <48858836+kiJiyeon@users.noreply.github.com>
2020-02-24 15:48:40 +0900

    추가 : selection 코드 작성 시작

--- MISO/Channel_maker_Selection.m
+++ MISO/Channel_maker_Selection.m
@@ -1,12 +1,12 @@
-function result = Noise_maker_MRC(N, RX_count, symbol)
-noise =sqrt(N/2)*randn(RX_count,length(symbol)) + 1i*(sqrt(N/2)*randn(RX_count,length(symbol)));      %���� ����
+function result = Noise_maker_Selection(N, TX_count, symbol)
+noise =sqrt(N/2)*randn(1,length(symbol)) + 1i*(sqrt(N/2)*randn(1,length(symbol)));      %���� ����
 %h = sqrt(0.5) * [randn(RX_count,length(symbol)) + 1i*randn(RX_count,length(symbol))];       % Rayleigh channel
 h = sqrt(0.5) * [randn(1,length(symbol)) + 1i*randn(1,length(symbol))];       % Rayleigh channel
-   
-temp = h;
-for mrc = 1:1:RX_count-1
-    h = [h;temp];
-end
+%    
+% temp = h;
+% for mrc = 1:1:RX_count-1
+%     h = [h;temp];
+% end
 
 % temp_2 = symbol;
 % for mrc = 1:1:RX_count-1
