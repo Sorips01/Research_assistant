07f575dfd56a9348c68478d66b3278b450fd4633
kiJiyeon <48858836+kiJiyeon@users.noreply.github.com>
2020-02-24 17:29:24 +0900

    기능 : MISO/ Selection기능 추가

--- MISO/Channel_maker_Selection.m
+++ MISO/Channel_maker_Selection.m
@@ -1,31 +1,28 @@
 function result = Noise_maker_Selection(N, TX_count, symbol)
-noise =sqrt(N/2)*randn(1,length(symbol)) + 1i*(sqrt(N/2)*randn(1,length(symbol)));      %���� ����
+noise =sqrt(N/2)*randn(TX_count,length(symbol)) + 1i*(sqrt(N/2)*randn(TX_count,length(symbol)));      %���� ����
 %h = sqrt(0.5) * [randn(RX_count,length(symbol)) + 1i*randn(RX_count,length(symbol))];       % Rayleigh channel
-h = sqrt(0.5) * [randn(1,length(symbol)) + 1i*randn(1,length(symbol))];       % Rayleigh channel
-%    
-% temp = h;
-% for mrc = 1:1:RX_count-1
-%     h = [h;temp];
-% end
+h = sqrt(0.5) * [randn(TX_count,length(symbol)) + 1i*randn(TX_count,length(symbol))];       % Rayleigh channel
 
-% temp_2 = symbol;
-% for mrc = 1:1:RX_count-1
-%     symbol = [symbol;temp_2];
-% end
+[~, index] = max(abs(h));
+h_Selected = zeros(1,length(h));
 
+for i = 1:1:length(symbol)
+    h_Selected(1,i) = h(index(1,i),i);              % select h
+end
 
-h_c = conj(h);      % h �ӷ����Ҽ� ����
-symbol_h = symbol.*h;
-symbol_noise=symbol_h+noise;
+[~, index] = max(abs(noise));
+noise_Selected = zeros(1,length(noise));
 
-%symbol_noise = zeros(1,length(symbol));
+for i = 1:1:length(symbol)
+    noise_Selected(1,i) = noise(index(1,i),i);        % select noise
+end
+
+h_c = conj(h_Selected);      % h_rms_max�� �ӷ����Ҽ� ����
+symbol_h = symbol.*h_Selected;   % hx
+symbol_noise = symbol_h + noise_Selected;    % hx + n
+
 
 symbol_noise = symbol_noise .* h_c;
-symbol_noise = sum(symbol_noise);
-h_square = zeros(1,length(h));
-
-h_square = h.*h_c;
-symbol_noise = symbol_noise ./ sum(h_square);
-
+symbol_noise = symbol_noise ./ (h_Selected .* h_c);
 result = symbol_noise;
 end
