a3543fda77d0cc9b3da7f17b9b7079c9e6e4c18a
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-02-24 18:29:36 +0900

    기능 : 출력 형식 수정

--- MISO/Rayleigh_fading_MISO/Channel_maker_MRT.m
+++ MISO/Rayleigh_fading_MISO/Channel_maker_MRT.m
@@ -1,12 +1,13 @@
 function result = Channel_maker_MRT(N, TX_count, symbol)
+N=TX_count*N;
 noise =sqrt(N/2)*randn(TX_count,length(symbol)) + 1i*(sqrt(N/2)*randn(TX_count,length(symbol)));      
 h = sqrt(0.5) * [randn(TX_count,length(symbol)) + 1i*randn(TX_count,length(symbol))];       % Rayleigh channel
 
 r = symbol.*h + noise;
 r = r.*conj(h);
 h_square = zeros(1,length(h));
-h_square = h.*conj(h);
-r = r ./ sum(h_square);
+h_square = sum(h.*conj(h));
+r = r ./ h_square;
 r = sum(r);
 result = r;
 end
