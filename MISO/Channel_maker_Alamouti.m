f0c33774a80407c5014f5ba95aa9dbf54be3dcf5
fora22 <48875566+fora22@users.noreply.github.com>
2020-02-24 18:17:12 +0900

    기능 : Alamouti 기능 추가
    기존 Demodulation과 RX_2_EGC_Rayleigh_fading_Channel_QPSK 파일을 삭제하고 새로운 행렬 연산 파일을 만듬

--- MISO/Channel_maker_Alamouti.m
+++ MISO/Channel_maker_Alamouti.m
@@ -1,31 +1,37 @@
-function result = Noise_maker_SC(N, RX_count, symbol)
-noise =sqrt(N/2)*randn(RX_count,length(symbol)) + 1i*(sqrt(N/2)*randn(RX_count,length(symbol)));      %���� ����
-%h = sqrt(0.5) * [randn(RX_count,length(symbol)) + 1i*randn(RX_count,length(symbol))];       % Rayleigh channel
-h = sqrt(0.5) * [randn(1,length(symbol)) + 1i*randn(1,length(symbol))];       % Rayleigh channel
+function result = Channel_maker_Alamouti(N, Tx_count, Rx_count,symbol)
+noise =sqrt(N/2)*(randn(Tx_count,Rx_count) + 1i*randn(Tx_count,Rx_count));      % Gaussian Noise
+h = sqrt(0.5) * (randn(Tx_count,Rx_count) + 1i*randn(Tx_count,Rx_count));       % Rayleigh channel
 
-temp = h;
-for mrc = 1:1:RX_count-1
-    h = [h;temp];
-end
+symbol = [symbol; -conj(symbol(2)) conj(symbol(1))];
 
-[~, index] = max(abs(h));
-h_SC = zeros(1,length(h));
-for i = 1:1:length(symbol)
-    h_SC(1,i) = h(index(1,i),i);              % select h
-end
-
-[~, index] = max(abs(noise));
-noise_SC = zeros(1,length(noise));
-for i = 1:1:length(symbol)
-    noise_SC(1,i) = noise(index(1,i),i);        % select noise
-end
-
-h_c = conj(h_SC);      % h_rms_max�� �ӷ����Ҽ� ����
-symbol_h = symbol.*h_SC;   % hx
-symbol_noise = symbol_h + noise_SC;    % hx + n
-
-
-symbol_noise = symbol_noise .* h_c;
-symbol_noise = symbol_noise ./ (h_SC .* h_c);
-result = symbol_noise;
+y = symbol*h + noise;
+y = [y(1) conj(y(2))];
+h_demo = [conj(h(1)) h(2); conj(h(2)) -h(1)];
+symbol_noise = h_demo*y;
+result = symbol_noise / sum(abs(h).^2);
+% temp = h;
+% for mrc = 1:1:TX_count-1
+%     h = [h;temp];
+% end
+% 
+% [~, index] = max(abs(h));
+% h_SC = zeros(1,length(h));
+% for i = 1:1:length(symbol)
+%     h_SC(1,i) = h(index(1,i),i);              % select h
+% end
+% 
+% [~, index] = max(abs(noise));
+% noise_SC = zeros(1,length(noise));
+% for i = 1:1:length(symbol)
+%     noise_SC(1,i) = noise(index(1,i),i);        % select noise
+% end
+% 
+% h_c = conj(h_SC);      % h_rms_max�� �ӷ����Ҽ� ����
+% symbol_h = symbol.*h_SC;   % hx
+% symbol_noise = symbol_h + noise_SC;    % hx + n
+% 
+% 
+% symbol_noise = symbol_noise .* h_c;
+% symbol_noise = symbol_noise ./ (h_SC .* h_c);
+% result = symbol_noise;
 end
