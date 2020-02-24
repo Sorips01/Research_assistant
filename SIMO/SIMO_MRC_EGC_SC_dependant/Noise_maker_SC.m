e14bff721e052ef454ecab04cfdaef5193bbade8
fora22 <48875566+fora22@users.noreply.github.com>
2020-02-24 13:39:36 +0900

    리팩토링 : 코드 수정
    SC에서 Noise가 최대 되는것으로 되지 않기 위해 Noise를 1개만듬

--- SIMO/SIMO_MRC_EGC_SC_dependant/Noise_maker_SC.m
+++ SIMO/SIMO_MRC_EGC_SC_dependant/Noise_maker_SC.m
@@ -1,5 +1,6 @@
 function result = Noise_maker_SC(N, RX_count, symbol)
-noise =sqrt(N/2)*randn(RX_count,length(symbol)) + 1i*(sqrt(N/2)*randn(RX_count,length(symbol)));      %���� ����
+%noise =sqrt(N/2)*randn(RX_count,length(symbol)) + 1i*(sqrt(N/2)*randn(RX_count,length(symbol)));      %���� ����
+noise_SC =sqrt(N/2)*randn(1,length(symbol)) + 1i*(sqrt(N/2)*randn(1,length(symbol)));      %���� ����
 %h = sqrt(0.5) * [randn(RX_count,length(symbol)) + 1i*randn(RX_count,length(symbol))];       % Rayleigh channel
 h = sqrt(0.5) * [randn(1,length(symbol)) + 1i*randn(1,length(symbol))];       % Rayleigh channel
 
@@ -14,11 +15,11 @@ for i = 1:1:length(symbol)
     h_SC(1,i) = h(index(1,i),i);              % select h
 end
 
-[value index] = max(abs(noise));
-noise_SC = zeros(1,length(noise));
-for i = 1:1:length(symbol)
-    noise_SC(1,i) = noise(index(1,i),i);        % select noise
-end
+% [value index] = max(abs(noise));
+% noise_SC = zeros(1,length(noise));
+% for i = 1:1:length(symbol)
+%     noise_SC(1,i) = noise(index(1,i),i);        % select noise
+% end
 
 h_c = conj(h_SC);      % h_rms_max�� �ӷ����Ҽ� ����
 symbol_h = symbol.*h_SC;   % hx
