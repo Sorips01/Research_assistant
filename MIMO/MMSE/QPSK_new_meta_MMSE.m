ec9f898a8895de5a53278c61bd6a1d5ce8a9673d
kiJiyeon <48858836+kiJiyeon@users.noreply.github.com>
2020-03-24 21:25:38 +0900

    리팩토링 : QPSK_new_meta _MMSE 코드수정
    함수호출부분, 에러리미트줌

--- MIMO/MMSE/QPSK_new_meta_MMSE.m
+++ MIMO/MMSE/QPSK_new_meta_MMSE.m
@@ -6,8 +6,9 @@ tic
 Tx = 2;
 Rx = 4;
 result = [];
+Error_Limit = 10^-8;
 
-for SNR=0:5:60
+for SNR = 0:5:60
     N = 10^(-0.1*SNR);
     error = zeros(1,1);
     trial = 0;
@@ -15,31 +16,32 @@ for SNR=0:5:60
         trial= trial + 1;
         
         % creat bit
-        bit = randi([0,1],2,1);
+        bit = randi([0,1],Tx,2);
         
         % creat symbol
-        symbol = (2*bit(1)-1 + j*(2*bit(2)-1))/sqrt(2);
-        
+        symbol = 2 * bit - 1;
+        symbol = symbol(:,1) + symbol(:,2) * 1j;
+                
         % modulation
-        h = (randn(1,1)+j*randn(1,1))/sqrt(2);
-        h = h * ones(Rx,1);
-        noise = (randn(Rx,1)+j*randn(Rx,1))*sqrt(N/2);
-        r = h*symbol+noise;
-        
         Demo_symbol = MMSE_Modulation(Tx, Rx, N, symbol);
-        
+        % ZF(Rx,Tx,N,symbol)
+        % MMSE(Rx,Tx,N,symbol)
         
         % demodulation
-        Demo_result(:,1) = real(Demo_symbol)>0; 
+        Demo_result(:,1) = real(Demo_symbol)>0; % MRC는 추후 변경하기
         Demo_result(:,2) = imag(Demo_symbol)>0;
         
         % count error
-        error = error + sum(abs(bit-Demo_result),'all');
+        error = error + sum(abs(bit-Demo_result), 'all');
+        
     end
     
-    error = error / (trial * 2);
+    error = error / (trial * 2 * Tx);
     fprintf("Tx 개수 : %d / Rx 개수 : %d / dB : %d / BER : %g \n", Tx, Rx, SNR, error);
-    result = [result;error];
+    if Error_Limit > error
+        break;
+    end
+    result = [result; error];
 end
 
 % save mat file
