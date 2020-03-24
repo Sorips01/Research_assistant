37a27048f13a018598b3ec78857805fa521ae67a
fora22 <48875566+fora22@users.noreply.github.com>
2020-03-24 21:01:00 +0900

    문서 : ZF 변조를 실행하는 파일 생성
    기본 파일  + ZF_modulation 파일 생성

--- MIMO/ZF/QPSK_new_meta_ZF.m
+++ MIMO/ZF/QPSK_new_meta_ZF.m
@@ -1,4 +1,4 @@
-mclear all;
+clear all;
 close all;
 format shortE;
 tic
@@ -15,44 +15,41 @@ for SNR=0:5:60
         trial= trial + 1;
         
         % creat bit
-        bit = randi([0,1],2,1);
+        bit = randi([0,1],Tx,2);
         
         % creat symbol
-        symbol = (2*bit(1)-1 + j*(2*bit(2)-1))/sqrt(2);
-        
+        symbol = 2 * bit - 1;
+        symbol = symbol(:,1) + symbol(:,2) * 1j;
+                
         % modulation
-        h = (randn(1,1)+j*randn(1,1))/sqrt(2);
-        h = h * ones(Rx,1);
-        noise = (randn(Rx,1)+j*randn(Rx,1))*sqrt(N/2);
-        r = h*symbol+noise;
+        Demo_symbol = ZF_Modulation(Tx, Rx, N, symbol);
         % ZF(Rx,Tx,N,symbol)
         % MMSE(Rx,Tx,N,symbol)
         
         % demodulation
-        Demo_result(1,1) = real(MRC)>0; % MRC는 추후 변경하기
-        Demo_result(2,1) = imag(MRC)>0;
+        Demo_result(:,1) = real(Demo_symbol)>0; % MRC는 추후 변경하기
+        Demo_result(:,2) = imag(Demo_symbol)>0;
         
         % count error
         error = error + sum(abs(bit-Demo_result));
     end
     
-    error = error / (trial*2);
+    error = error / (trial * 2 * Tx);
     fprintf("Tx 개수 : %d / Rx 개수 : %d / dB : %d / BER : %g \n", Tx, Rx, SNR, error);
-    result = [result;error];
+    result = [result; error];
 end
 
 % save mat file
 ZF_result = result;
-cd ..
-cd mat_Rayleigh_fading_MISO % 폴더명
 
-if (exist('MRT_QPSK_Rayleigh_MISO.mat', 'file') > 0) 
-    save('MRT_QPSK_Rayleigh_MISO.mat', 'ZF_result', '-append'); 
+cd mat_folder % 폴더명
+
+if (exist('QPSK_new_meta_ZF.mat', 'file') > 0) 
+    save('QPSK_new_meta_ZF.mat', 'ZF_result', '-append'); 
 else
-    save('MRT_QPSK_Rayleigh_MISO.mat', 'ZF_result');
+    save('QPSK_new_meta_ZF.mat', 'ZF_result');
 end
 
 cd ..
-cd Rayleigh_fading_MISO
 
 toc
