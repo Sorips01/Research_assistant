e7f67204a9c30c2537be36e6aa47eef12707d27b
fora22 <48875566+fora22@users.noreply.github.com>
2020-03-26 14:14:18 +0900

    기능 : 최적화 완료

--- MIMO/Graph_ZF_MMSE.m
+++ MIMO/Graph_ZF_MMSE.m
@@ -1,18 +1,19 @@
 clear all;
 close all;
 
-cd ZF
-run('QPSK_new_meta_ZF.m');
-cd ..
-cd MMSE
-run('QPSK_new_meta_MMSE.m');
-cd ..
-clear all;
+% cd ZF
+% run('QPSK_new_meta_ZF.m');
+% cd ..
+% cd MMSE
+% run('QPSK_new_meta_MMSE.m');
+% cd ..
+% clear all;
 
 x=0:5:60;
 LineWidth = 3;
 lim_y = 1E-8;
 
+% loading mat file
 cd ZF
 load(fullfile(pwd, '\mat_folder\QPSK_new_meta_ZF.mat'));
 cd ..
@@ -20,6 +21,10 @@ cd MMSE
 load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE.mat'));
 cd ..
 load(fullfile(pwd, '\RX_3_MRC_QPSK_Rayleigh_SIMO'));
+
+% length of result normalize
+ZF_result = length_normalize(x, ZF_result);
+MMSE_result = length_normalize(x, MMSE_result);
 
 % ZF / MMSE / MRC
 f1 = figure;
@@ -29,10 +34,8 @@ semilogy(x,MMSE_result, 'LineWidth', LineWidth);
 hold on;
 semilogy(x,QPSK_BER_MRC_RX_3, 'LineWidth', LineWidth);
 hold on;
-xlim([0 60]);
+% xlim([0 60]);
 ylim([lim_y 1]);
-ylabel(f1,'BER ---->');
-xlabel(f1, 'SNR ---->');
 ylabel('BER ---->');
 xlabel('SNR ---->');
 legend('ZF','MMSE','MRC');
