8a4e572c6d964760fad2b26e732c4f4164c5504c
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-05-01 20:15:32 +0900

    기능 : SIC 코드 수정 (최종)
    OSIC의 코드가 잘못 쓰여짐

--- MIMO/SIC/SIC.m
+++ MIMO/SIC/SIC.m
@@ -1,16 +1,5 @@
-<<<<<<< Updated upstream
-function [result,h] = SIC(r_result,h,r)
-
-[~,maxIndex] = max(sum(abs(h)));
-r=r-h(:,maxIndex)*r_result(maxIndex,1);
-result = r;
-
-h(:,maxIndex) = [];
-
-=======
 function [result,h] = SIC(r_result,h,r,i)
 r=r-h(:,1)*r_result(1,1);
 result = r;
 h(:,1) = [];
->>>>>>> Stashed changes
 end
