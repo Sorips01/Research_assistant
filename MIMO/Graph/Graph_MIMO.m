25e53afe1da0ed3d12b2874ce9ac632013edb0f2
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-05-01 22:06:06 +0900

    기능 : MIMO 그래프 코드에 모든 mat파일 추가

--- MIMO/Graph/Graph_MIMO.m
+++ MIMO/Graph/Graph_MIMO.m
@@ -10,21 +10,40 @@ close all;
 % clear all;
 
 x=0:5:60;
-LineWidth = 3;
+LineWidth = 1;
 lim_y = 1E-8;
 
 % loading mat file
 load(fullfile(pwd, '\mat_folder\QPSK_new_meta_ML.mat'));
 load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE.mat'));
 load(fullfile(pwd, '\mat_folder\QPSK_new_meta_ZF.mat'));
-load(fullfile(pwd, '\mat_folder\QPSK_new_meta_ZF_OSIC.mat'));
-load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE_OSIC.mat'));
+
 load(fullfile(pwd, '\mat_folder\QPSK_new_meta_ZF_SIC.mat'));
 load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE_SIC.mat'));
 
+load(fullfile(pwd, '\mat_folder\QPSK_new_meta_OSIC_ZF.mat'));
+load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE_OSIC.mat'));
+
 % length of result normalize
-%ZF_result = length_normalize(x, ZF_result);
-%MMSE_result = length_normalize(x, MMSE_result);
+ZF_result_2x2 = length_normalize(x, ZF_result_2x2);
+ZF_result_4x4 = length_normalize(x, ZF_result_4x4);
+MMSE_result2x2 = length_normalize(x, MMSE_result2x2);
+MMSE_result4x4 = length_normalize(x, MMSE_result4x4);
+ML_result_2x2 = length_normalize(x, ML_result_2x2);
+ML_result_4x4 = length_normalize(x, ML_result_4x4);
+
+ZF_SIC_result_2x2 = length_normalize(x, ZF_SIC_result_2x2);
+ZF_SIC_result_4x4 = length_normalize(x, ZF_SIC_result_4x4);
+MMSE_SIC_result_2x2 = length_normalize(x, MMSE_SIC_result_2x2);
+MMSE_SIC_result_4x4 = length_normalize(x, MMSE_SIC_result_4x4);
+
+% MMSE-OSIC
+OSIC_result_2x2 = length_normalize(x, OSIC_result_2x2); 
+OSIC_result_4x4 = length_normalize(x, OSIC_result_4x4);
+
+OSIC_ZF_result_2x2 = length_normalize(x, OSIC_ZF_result_2x2); 
+OSIC_ZF_result_4x4 = length_normalize(x, OSIC_ZF_result_4x4);
+
 
 % ZF / MMSE / ML : 2x2
 f1 = figure;
@@ -42,16 +61,17 @@ semilogy(x, MMSE_SIC_result_2x2, 'LineWidth', LineWidth);
 hold on;
 
 % ZF-OSIC / MMSE-OSIC 2x2
-semilogy(x, ZF_result, 'LineWidth', LineWidth);
+semilogy(x, ZF_SIC_result_2x2, 'LineWidth', LineWidth);
 hold on;
-semilogy(x, MMSE_result, 'LineWidth', LineWidth);
+semilogy(x, MMSE_SIC_result_2x2, 'LineWidth', LineWidth);
 hold on;
 
 % xlim([0 60]);
 ylim([lim_y 1]);
 ylabel('BER ---->');
 xlabel('SNR ---->');
-legend('ZF','MMSE','ML');
+legend('ZF','MMSE','ML','ZF-SIC','MMSE-SIC','ZF-OSIC','MMSE-OSIC');
+title('MIMO 2x2');
 
 % ZF / MMSE / ML : 4x4
 f2 = figure;
@@ -62,12 +82,6 @@ hold on;
 semilogy(x, ML_result_4x4, 'LineWidth', LineWidth);
 hold on;
 
-% xlim([0 60]);
-ylim([lim_y 1]);
-ylabel('BER ---->');
-xlabel('SNR ---->');
-legend('ZF','MMSE','ML');
-
 % ZF-SIC / MMSE-SIC 4x4
 semilogy(x, ZF_SIC_result_4x4, 'LineWidth', LineWidth);
 hold on;
@@ -75,7 +89,13 @@ semilogy(x, MMSE_SIC_result_4x4, 'LineWidth', LineWidth);
 hold on;
 
 % ZF-OSIC / MMSE-OSIC 4x4
-semilogy(x, ZF_result, 'LineWidth', LineWidth);
+semilogy(x, OSIC_ZF_result_4x4, 'LineWidth', LineWidth);
 hold on;
-semilogy(x, MMSE_result, 'LineWidth', LineWidth);
+semilogy(x, OSIC_result_4x4, 'LineWidth', LineWidth);
 hold on;
+
+ylim([lim_y 1]);
+ylabel('BER ---->');
+xlabel('SNR ---->');
+legend('ZF','MMSE','ML','ZF-SIC','MMSE-SIC','ZF-OSIC','MMSE-OSIC');
+title('MIMO 4x4');
