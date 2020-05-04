6fdb99930c27a16c622b9602b84f3c8037f6ba11
fora22 <48875566+fora22@users.noreply.github.com>
2020-05-04 14:56:21 +0900

    문서 : 함수 이름 변경에 따른 코드 수정

--- MIMO/OSIC/OSIC_MMSE/OSIC_MMSE.m
+++ MIMO/OSIC/OSIC_MMSE/OSIC_MMSE.m
@@ -1,4 +1,4 @@
-function [result,h] = OSIC(r_result,h,r)
+function [result,h] = OSIC_MMSE(r_result,h,r)
 r=r-h(:,1)*r_result(1,1);
 result = r;
 h(:,1) = [];
--- MIMO/SIC/MMSE/SIC_Demodulation.m
+++
@@ -1,5 +0,0 @@
-function [sicResult] = SIC_Demodulation(Demo_symbol,h)
-
-sicResult = Demo_symbol - h(:,1)*Demo_symbol(1)
-
-end
