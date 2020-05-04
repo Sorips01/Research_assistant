62c665fb95a4c0bf9751cf7a5de34a288e9310fb
fora22 <48875566+fora22@users.noreply.github.com>
2020-05-04 17:42:05 +0900

    문서 : ISDIC 파일 만듬
    논문에 나온 과정까지는 만듬
    loop 돌때 끝나는 결정기준을 만들어야함

---
+++ MIMO/ISDIC/QPSK_new_meta_MMSE_ISDIC.m
@@ -0,0 +1,108 @@
+clear all;
+close all;
+format shortE;
+tic
+
+Tx = 3;
+Rx = 3;
+result = [];
+Error_Limit = 10^-5;
+
+for SNR = 0:5:60
+    N = 2*10^(-0.1*SNR);
+    error = zeros(1,1);
+    trial = 0;
+    while error < 1000      
+        trial= trial + 1;
+        
+        % creat bit
+        bit = randi([0,1],Tx,2);
+        
+        % creat symbol
+        symbol = 2 * bit - 1;
+        symbol = symbol(:,1) + symbol(:,2) * 1j;
+        
+        h = (randn(Rx,Tx) + 1j * randn(Rx,Tx))/sqrt(2);
+        noise = (randn(Rx,1) + 1j * randn(Rx,1)) * sqrt(N/2);
+        r = h * symbol + noise;
+        
+        % create vector
+        s = zeros(Tx,1);
+        v = ones(Tx,1);
+        
+        
+        
+        % loop start
+        r_parallel(:,:,1) = r - (h(:,2) * s(2)) - (h(:,3) * s(3));
+        r_parallel(:,:,2) = r - (h(:,1) * s(1)) - (h(:,3) * s(3));
+        r_parallel(:,:,3) = r - (h(:,1) * s(1)) - (h(:,2) * s(2));
+        
+        for i = 1:1:Tx
+            D(:,:,i) = v(i) * eye(Tx);
+        end
+        
+        for i = 1:1:Tx
+            f(:,:,i) = conj(h(:,i).') * (h * D(:,:,i) * conj(h.') + N * eye(Rx));
+        end
+        
+        for i = 1:1:Tx
+            b(:,:,i) = f(:,:,1) * h(:,i);
+        end
+        
+        a_q = [1+1i, 1-1i, -1+1i, -1-1i];
+        
+        for i = 1:1:Tx
+            p(:,:,i) = exp((-1 * abs(f(:,:,i) * r_parallel(:,:,i) - a_q * b(:,:,i) ) / (b(:,:,i) * (1 - b(:,:,i)) ) ) ); % a_q 없음 추가해야됨
+        end
+        
+        for i = 1:1:Tx
+           s(i) = sum(a_q .* p(:,:,i)) / sum(p(:,:,i));
+        end
+        
+        for i = 1:1:Tx
+            v(i) = sum(abs(a_q - s(i)).^2 .* p(:,:,i)) / sum(p(:,:,i));
+        end
+        
+        
+        
+        
+        Demo_symbol = [];
+                
+        % modulation
+        Demo_symbol = MMSE_Modulation(Tx, Rx, N, symbol);
+        % ZF(Rx,Tx,N,symbol)
+        % MMSE(Rx,Tx,N,symbol)
+        
+        % demodulation
+        Demo_result(:,1) = real(Demo_symbol)>0; % MRC는 추후 변경하기
+        Demo_result(:,2) = imag(Demo_symbol)>0;
+        
+        % count error
+        error = error + sum(abs(bit-Demo_result), 'all');
+        
+    end
+    
+    error = error / (trial * 2 * Tx);
+    fprintf("Tx 개수 : %d / Rx 개수 : %d / dB : %d / BER : %g \n", Tx, Rx, SNR, error);
+    if Error_Limit > error
+        break;
+    end
+    result = [result error];
+end
+
+% save mat file
+MMSE_result4x4 = result;
+
+folderName = 'QPSK_new_meta_MMSE.mat'
+fileName = 'MMSE_result4x4'
+cd mat_folder % 폴더명
+
+if (exist(folderName, 'file') > 0) 
+    save(folderName, fileName, '-append'); 
+else
+    save(folderName, fileName);
+end
+
+cd ..
+
+toc
