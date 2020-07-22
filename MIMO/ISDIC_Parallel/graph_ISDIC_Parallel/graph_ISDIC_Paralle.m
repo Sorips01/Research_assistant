9d4ef8a0de50f24bc2e42dc7304f00637dad8dc6
팽대원 <fora22@kyonggi.ac.kr>
2020-07-22 17:22:48 +0900

    graph 코드 수정 완료

--- MIMO/ISDIC_Parallel/graph_ISDIC_Parallel/graph_ISDIC_Paralle.m
+++ MIMO/ISDIC_Parallel/graph_ISDIC_Parallel/graph_ISDIC_Paralle.m
@@ -1,11 +1,14 @@
 clear all
-
-load('QPSK_new_meta_MMSE_ISDIC_Parallel_3x3.mat')
+cd ..
+cd mat_folder
+load('QPSK_new_meta_MMSE_ISDIC_Parallel_8x8.mat')
+cd ..
+cd graph_ISDIC_Parallel
 x=0:5:60;
 LineWidth = 1;
 lim_y = 1E-8;
 
-y = MMSE_ISDIC_Parallel_result3x3;
+y = QPSK_new_meta_MMSE_ISDIC_Parallel_result_8x8;
 
 y = length_normalize(x, y);
 semilogy(x,y, 'LineWidth', LineWidth);
