4e49017db6d1a39b2ff8aec95b4ca1bd9fdcce39
kiJiyeon <48858836+kiJiyeon@users.noreply.github.com>
2020-12-04 12:27:35 +0900

    기능 : 8x8 mat파일 그래프 그리는 기능 추가

--- Thesis/Jiyeon/Graph/Graph_IterationChanged.m
+++ Thesis/Jiyeon/Graph/Graph_IterationChanged.m
@@ -8,30 +8,32 @@ LineWidth = 1;
 lim_y = 1E-8;
 
 % loading mat file
-% load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE_ISDIC_Serial_result_1.mat'));
-load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE_ISDIC_Serial_3x3.mat'));
-% load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE_ISDIC_Serial_result_4.mat'));
-% load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE_ISDIC_Serial_result_5.mat'));
-% 
+% load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE_ISDIC_Serial_3x3.mat'));
+load(fullfile(pwd, '\mat_folder\QPSK_new_meta_MMSE_ISDIC_Serial_8x8.mat'));
+
 % length of result normalize
 % ISDIC_Serial_result_1 = length_normalize(x, ISDIC_Serial_result_1);
 ISDIC_Serial_result_maxP = length_normalize(x, QPSK_new_meta_MMSE_ISDIC_Serial_result_maxP);
-ISDIC_Serial_result_absOfSubstraction = length_normalize(x, ISDIC_Serial_result_absOfSubstraction);
+% ISDIC_Serial_result_absOfSubstraction = length_normalize(x, ISDIC_Serial_result_absOfSubstraction);
 
-% ZF / MMSE / ML : 2x2
-f1 = figure;
-% semilogy(x,ISDIC_Serial_result_1, 'LineWidth', LineWidth);
+% % ISDIC 3x3
+% f1 = figure;
+% semilogy(x,ISDIC_Serial_result_maxP, 'LineWidth', LineWidth);
 % hold on;
+% % semilogy(x,ISDIC_Serial_result_absOfSubstraction, 'LineWidth', LineWidth);
+% % hold on;
+
+% ISDIC 8x8
+f1 = figure;
 semilogy(x,ISDIC_Serial_result_maxP, 'LineWidth', LineWidth);
 hold on;
-semilogy(x,ISDIC_Serial_result_absOfSubstraction, 'LineWidth', LineWidth);
-hold on;
-% semilogy(x,ISDIC_Serial_result_5, 'LineWidth', LineWidth);
+% semilogy(x,ISDIC_Serial_result_absOfSubstraction, 'LineWidth', LineWidth);
 % hold on;
+
 
 % xlim([0 60]);
 ylim([lim_y 1]);
 ylabel('BER ---->');
 xlabel('SNR ---->');
-legend('maxP','absOfSubstraction','Iteration 5');
-title('ISDIC Serial 3x3');
+legend('maxP','absOfSubstraction');
+title('ISDIC Serial');
