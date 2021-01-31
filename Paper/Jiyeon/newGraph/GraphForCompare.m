105bc40b2c967ca85b6e56746c7973e1fd84be27
kiJiyeon <48858836+kiJiyeon@users.noreply.github.com>
2021-01-31 17:09:54 +0900

    16x16 다 돌림

--- Paper/Jiyeon/newGraph/GraphForCompare.m
+++ Paper/Jiyeon/newGraph/GraphForCompare.m
@@ -3,40 +3,70 @@ LineWidth = 5;
 lim_y = 1E-6;
 
 %% Tx x Rx = 4 x 4
-%
+
+% x = -2:2:16;
+% w0 = load(fullfile(pwd, '\matFolder\ISDIC_4x4_Iteration_1.mat'));
 % w1 = load(fullfile(pwd, '\matFolder\ISDIC_4x4_Iteration_0.5.mat'));
 % w2 = load(fullfile(pwd, '\matFolder\ISDIC_4x4_Iteration_0.6.mat'));
 % w3 = load(fullfile(pwd, '\matFolder\ISDIC_4x4_Iteration_0.7.mat'));
 % w4 = load(fullfile(pwd, '\matFolder\ISDIC_4x4_Iteration_0.8.mat'));
 % w5 = load(fullfile(pwd, '\matFolder\ISDIC_4x4_Iteration_0.9.mat'));
-
+% 
+% %
+% w0 = reshape(w0.result,5,10);
+% w1 = reshape(w1.result,5,10);
+% w2 = reshape(w2.result,5,10);
+% w3 = reshape(w3.result,5,10);
+% w4 = reshape(w4.result,5,10);
+% w5 = reshape(w5.result,5,10);
 
 %% Tx x Rx = 8 x 8
 % %
-x = 0:2:10;
-x2 = 0:2:8;
+% x = -2:2:8;
+% 
+% w0 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_1.mat'));
+% w1 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.5.mat'));
+% w2 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.6.mat'));
+% w3 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.7.mat'));
+% w4 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.8.mat'));
+% w5 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.9.mat'));
+% %
+% %
+% w0 = reshape(w0.result,5,6);
+% w1 = reshape(w1.result,5,6);
+% w2 = reshape(w2.result,5,6);
+% w3 = reshape(w3.result,5,6);
+% w4 = reshape(w4.result,5,6);
+% w5 = reshape(w5.result,5,6);
 
-w1 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.5.mat'));
-w2 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.6.mat'));
-w3 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.7.mat'));
-w4 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.8.mat'));
-w5 = load(fullfile(pwd, '\matFolder\ISDIC_8x8_Iteration_0.9.mat'));
+%% Tx x Rx = 16 x 16
+%
+x = -2:2:4;
+
+% w0 = load(fullfile(pwd, '\matFolder\ISDIC_16x16_Iteration_1.mat'));
+w1 = load(fullfile(pwd, '\matFolder\ISDIC_16x16_Iteration_0.5.mat'));
+w2 = load(fullfile(pwd, '\matFolder\ISDIC_16x16_Iteration_0.6.mat'));
+w3 = load(fullfile(pwd, '\matFolder\ISDIC_16x16_Iteration_0.7.mat'));
+w4 = load(fullfile(pwd, '\matFolder\ISDIC_16x16_Iteration_0.8.mat'));
+w5 = load(fullfile(pwd, '\matFolder\ISDIC_16x16_Iteration_0.9.mat'));
 %
 %
-w1 = reshape(w1.result,5,6);
-w2 = reshape(w2.result,5,6);
-w3 = reshape(w3.result,5,6);
-w4 = reshape(w4.result,5,5);
-w5 = reshape(w5.result,5,5);
-
+% w0 = reshape(w0.result,5,4);
+w1 = reshape(w1.result,5,4);
+w2 = reshape(w2.result,5,4);
+w3 = reshape(w3.result,5,4);
+w4 = reshape(w4.result,5,4);
+w5 = reshape(w5.result,5,4);
 %% cross-platform
 
 
 ISDIC_Serial_result_05 = length_normalize(x, w1);
 ISDIC_Serial_result_06 = length_normalize(x, w2);
 ISDIC_Serial_result_07 = length_normalize(x, w3);
-ISDIC_Serial_result_08 = length_normalize(x2, w4);
-ISDIC_Serial_result_09 = length_normalize(x2, w5);
+ISDIC_Serial_result_08 = length_normalize(x, w4);
+ISDIC_Serial_result_09 = length_normalize(x, w5);
+% ISDIC_Serial_result_1 = length_normalize(x, w0);
+
 
 f1 = figure;
 semilogy(x,ISDIC_Serial_result_05(1,:), 'LineWidth', LineWidth);
@@ -44,13 +74,17 @@ hold on;
 semilogy(x,ISDIC_Serial_result_05(4,:), 'LineWidth', LineWidth);
 hold on;
 
+ylim([lim_y 1]);
+ylabel('BER ---->');
+xlabel('SNR ---->');
+legend('iteration = 1','iteration = 4','iteration = 8')
+title('maxP = 0.5');
+
 f2 = figure;
 semilogy(x,ISDIC_Serial_result_06(1,:), 'LineWidth', LineWidth);
 hold on;
 semilogy(x,ISDIC_Serial_result_06(4,:), 'LineWidth', LineWidth);
 hold on;
-
-
 
 
 ylim([lim_y 1]);
@@ -66,21 +100,18 @@ hold on;
 semilogy(x,ISDIC_Serial_result_07(4,:), 'LineWidth', LineWidth);
 hold on;
 
-
 ylim([lim_y 1]);
 ylabel('BER ---->');
 xlabel('SNR ---->');
 legend('iteration = 1','iteration = 4','iteration = 8')
 title('maxP = 0.7');
 
+
 f4 = figure;
-semilogy(x2,ISDIC_Serial_result_08(1,:), 'LineWidth', LineWidth);
+semilogy(x,ISDIC_Serial_result_08(1,:), 'LineWidth', LineWidth);
 hold on;
-semilogy(x2,ISDIC_Serial_result_08(4,:), 'LineWidth', LineWidth);
+semilogy(x,ISDIC_Serial_result_08(4,:), 'LineWidth', LineWidth);
 hold on;
-
-
-
 
 ylim([lim_y 1]);
 ylabel('BER ---->');
@@ -88,11 +119,11 @@ xlabel('SNR ---->');
 legend('iteration = 1','iteration = 4','iteration = 8');
 title('maxP = 0.8');
 %
-
+% 
 f5 = figure;
-semilogy(x2,ISDIC_Serial_result_09(1,:), 'LineWidth', LineWidth);
+semilogy(x,ISDIC_Serial_result_09(1,:), 'LineWidth', LineWidth);
 hold on;
-semilogy(x2,ISDIC_Serial_result_09(4,:), 'LineWidth', LineWidth);
+semilogy(x,ISDIC_Serial_result_09(4,:), 'LineWidth', LineWidth);
 hold on;
 
 
@@ -102,5 +133,16 @@ xlabel('SNR ---->');
 legend('iteration = 1','iteration = 4','iteration = 8');
 title('maxP = 0.9');
 
+f1 = figure;
+semilogy(x,ISDIC_Serial_result_1(1,:), 'LineWidth', LineWidth);
+hold on;
+semilogy(x,ISDIC_Serial_result_1(4,:), 'LineWidth', LineWidth);
+hold on;
+
+ylim([lim_y 1]);
+ylabel('BER ---->');
+xlabel('SNR ---->');
+legend('iteration = 1','iteration = 4','iteration = 8')
+title('maxP = 1');
 
 
