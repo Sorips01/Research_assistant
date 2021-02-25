57675ca453080bbb1c54af8b941854612bd89cd5
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2021-02-26 00:53:15 +0900

    논문 최종 수정

--- Paper/Areum/Graph/Graph_Grouping.m
+++ Paper/Areum/Graph/Graph_Grouping.m
@@ -22,12 +22,12 @@ d = load(fullfile(pwd, '\result\ISDIC_8x8_Grouping_8_ordering_3.mat'));
 serial_8 = load(fullfile(pwd, '\result\ISDIC_8x8_ordering_3_original.mat'));
 MMSE_8x8 = load(fullfile(pwd, '\result\QPSK_new_meta_MMSE.mat'));
 
-e = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_1_ordering_2.mat'));
-f = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_2_ordering_2.mat'));
-g = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_4_ordering_2.mat'));
-h = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_8_ordering_2.mat'));
-i = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_16_ordering_2.mat'));
-serial_16 = load(fullfile(pwd, '\result\ISDIC_16x16.mat'));
+e = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_1_ordering_3.mat'));
+f = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_2_ordering_3.mat'));
+g = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_4_ordering_3.mat'));
+h = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_8_ordering_3.mat'));
+i = load(fullfile(pwd, '\result\ISDIC_16x16_Grouping_16_ordering_3.mat'));
+serial_16 = load(fullfile(pwd, '\result\ISDIC_16x16_ordering_3_original.mat'));
 
 for z = 5:5:45
     result_a = [result_a a.result(1,z)];
@@ -42,21 +42,24 @@ end
 % result_d = [result_d 0.00435055];
 % result_serial_8 = [result_serial_8 1.93463e-06];
 % MMSE_8x8.MMSE_result8x8(:,7)=[];
- MMSE_8x8.MMSE_result16x16(:,6:8)=[];
-for z = 5:5:25
-    result_serial_16 = [result_serial_16 serial_16.result(1,z)];
+MMSE_8x8.MMSE_result16x16(:,8:9)=[];
+for z = 5:5:35
+    
     result_f = [result_f f.result(1,z)];
     result_e = [result_e e.result(1,z)];
     result_g = [result_g g.result(1,z)];
     result_h = [result_h h.result(1,z)];
+    result_i = [result_i i.result(1,z)];
+    result_serial_16 = [result_serial_16 serial_16.result(1,z)];
 end
 
 % result_e = [result_e 0 0];
 % result_f = [result_f 0 0 0];
-% result_serial_16 = [result_serial_16 0 0 0];
-for z = 5:5:25 
-    result_i = [result_i i.result(1,z)];
-end
+
+% for z = 5:5:25 
+%     
+% end
+% result_serial_16 = [result_serial_16 0 0 ];
 
 figure(1);
 semilogy(x,result_serial_8); hold on;
@@ -73,17 +76,17 @@ title('ISDIC Serial 8x8');
 
 legend({'ISDIC serial','MMSE','Grouping 1','Grouping 2','Grouping 4','Grouping 8'},'Location','southwest')
 
-x=-8:4:8;
+x=-6:2:6;
 figure(2);
+semilogy(x,result_serial_16); hold on;
+semilogy(x,MMSE_8x8.MMSE_result16x16); hold on;
 semilogy(x,result_e); hold on;
 semilogy(x,result_f); hold on;
 semilogy(x,result_g); hold on;
 semilogy(x,result_h); hold on;
 semilogy(x,result_i); hold on;
-semilogy(x,result_serial_16); hold on;
-semilogy(x,MMSE_8x8.MMSE_result16x16); hold on;
-ylabel('BER ---->');
-xlabel('SNR ---->');
-title('ISDIC Serial 16x16');
 
-legend({'Grouping 1','Grouping 2','Grouping 4','Grouping 8','Grouping 16','ISDIC serial','MMSE'},'Location','southwest')
+ylabel('BER');
+xlabel('SNR [dB]');
+
+legend({'ISDIC serial','MMSE','Grouping 1','Grouping 2','Grouping 4','Grouping 8','Grouping 16'},'Location','southwest')
