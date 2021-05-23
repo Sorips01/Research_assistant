9fc817960a3015f408c62afad4d5ab561175fbc0
kijiyeon <rlwldus305@naver.com>
2021-05-23 13:46:09 +0900

    lpf padding 앞뒤추가

--- Assignment/Jiyeon/디지털 신호처리 프로젝트/FIR_LPF_Filtering.m
+++ Assignment/Jiyeon/디지털 신호처리 프로젝트/FIR_LPF_Filtering.m
@@ -3,15 +3,15 @@
 function [result, filterOrder] = FIR_LPF_Filtering(x,y)
 
 %% moving average filter
-order = 9;
+order = 5;
 y_n = zeros(size(x));
 [row,col] = size(x);
 
 for i= 1:col 
-    inputX = [zeros(1,order-1),x(i,:)];
+    inputX = [zeros(1,order-1),x(i,:),zeros(1,order-1)];
     for j = 1:row 
-        for k = 1:order
-            y(i,j) = y(i,j)+inputX(j+k-1);
+        for k = 1:order-1
+            y(i,j) = y(i,j)+inputX(j+k);
         end
         y_n(i,j)= (1/order)*y(i,j);
     end
--- Assignment/Jiyeon/디지털 신호처리 프로젝트/main.m
+++ Assignment/Jiyeon/디지털 신호처리 프로젝트/main.m
@@ -6,11 +6,11 @@
 y=uint8(zeros(size(x)));
 
 [y,order] = FIR_LPF_Filtering(x,y);
-[z,b] = FIR_HPF_Filtering(x,y);
+% [z,b] = FIR_HPF_Filtering(x,y);
 
 %% save files 
 fileNameLPF = strcat('lena',string(order),'_LPF.jpg'); 
 imwrite(y,map,fileNameLPF);
 
-fileNameHPF = strcat('lena',string(b),'_HPF.jpg'); 
-imwrite(z,map,fileNameHPF);
+% fileNameHPF = strcat('lena',string(b),'_HPF.jpg'); 
+% imwrite(z,map,fileNameHPF);
