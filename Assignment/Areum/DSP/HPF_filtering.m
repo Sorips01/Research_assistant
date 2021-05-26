5155df8d3aed8769848fe70850dac35fd4334dbf
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2021-05-26 16:46:39 +0900

    HPF filtering

---
+++ Assignment/Areum/DSP/HPF_filtering.m
@@ -0,0 +1,24 @@
+[x, map] = imread('lena.png');
+y = uint8(zeros(size(x)));
+
+[Rows,Cols] = size(x);
+z = [];
+b = 70;
+
+for i = 1:Rows
+    z(i,:) = [zeros(1,1), x(i,:), zeros(1,1)];
+end
+
+z = [zeros(1,258); z ; zeros(1,258)];
+
+for i = 2:Rows
+    for j = 2:Cols
+        y(i,j) = 3*z(i,j)-(z(i+1,j) + z(i-1,j) + z(i,j+1) + z(i,j-1)) + ...
+        0.25 * (z(i+1,j+1) + z(i+1,j-1) + z(i-1,j+1) + z(i-1, j-1));
+        
+        k(i,j) = x(i,j) + b * y(i,j);
+    end
+end
+
+fileName = strcat('HPF_',string(b),'.jpg');
+imwrite(k,map,fileName);
