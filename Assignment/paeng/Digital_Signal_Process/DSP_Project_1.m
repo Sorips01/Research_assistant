c98fb4bcbf1d37a9a2a65f03b32c47c19e7355be
fora22 <48875566+fora22@users.noreply.github.com>
2021-05-31 14:13:31 +0900

    DSP Update

--- Assignment/paeng/Digital_Signal_Process/DSP_Project_1.m
+++ Assignment/paeng/Digital_Signal_Process/DSP_Project_1.m
@@ -5,41 +5,62 @@ clear all;
 y = uint8(zeros(size(x)));
 [xRow, xCol] = size(x);
 
-t= [1:xRow];
-n = length(t);
-f = -n/2:(n/2)-1;
-point = 10;
+n = xRow;
+t = 1:n;
+
+point = 7;
 
 %% Zero Padding
-% for ii =1:xRow
-%     input = [zeros(1, point-1), x(ii,:)];
-%     for jj = 1:xCol
-%         for kk = 1:point
-%             y(ii,jj) = y(ii,jj) + input(jj + kk - 1);
-%         end
-%         y(ii,jj) = y(ii,jj) / point;
-%     end
-% end
-%% No Zero Padding
-for ii = 1:(xRow)
-    for jj = point:(xCol)
-        for kk = 0:(point-1)
-            y(ii,jj) = y(ii,jj) + x(ii,jj - kk);
+for ii =1:xRow
+    input = [zeros(1, point-1), x(ii,:)];
+    for jj = 1:xCol
+        for kk = 1:point
+            y(ii,jj) = y(ii,jj) + input(jj + kk - 1);
         end
         y(ii,jj) = y(ii,jj) / point;
     end
 end
 
-for ii = 1:(xRow)
-   for jj = 1:(point-1)
-       for kk = 0:(jj-1)
-            y(ii,jj) = y(ii,jj) + x(ii,jj - kk);
-       end
-       y(ii,jj) = y(ii,jj) / point;
-   end
-end
-
+gx = reshape(x, 1,[]);  % Graph X
+gy = reshape(y, 1,[]);  % Graph y
 %% file save
-fileName = strcat('LenaOutuput_',string(point),'_point_LPF.jpg');
+fileName = strcat('LPF/LenaOutuput_',string(point),'_point_LPF.jpg');
 imwrite(y, map, fileName);
 
+% matFileName_gx = strcat(pwd,'\data\', string(point), '_point_gx.mat');
+% matFileName_gy = strcat(pwd,'\data\', string(point), '_point_gy.mat');
+% save(matFileName_gx, 'gx');
+% save(matFileName_gy, 'gy');
+
+%% Origin & 3-Point Graph
+% load(fullfile(pwd, '\data\3_point_gx.mat'));
+% load(fullfile(pwd, '\data\3_point_gy.mat'));
+% subplot(2,1,1);
+% plot(1:length(gx), gx, 1:length(gy), gy);
+% xlim([0 inf]);
+% legend('Origin', '3-Point');
+
+% subplot(2,1,2);
+% fx = fftshift(fft(gx));
+% fy = fftshift(fft(gy));
+% f = -(length(gx))/2:(length(gx)/2)-1;
+% plot(f,abs(fx), f, abs(fy));
+% xlim([-inf inf]);
+% legend('Origin', '3-Point');
+
+%% Origin & 7-Point Graph
+% load(fullfile(pwd, '\data\7_point_gx.mat'));
+% load(fullfile(pwd, '\data\7_point_gy.mat'));
+% 
+% subplot(2,1,1);
+% plot(1:length(gx), gx, 1:length(gy), gy);
+% xlim([0 inf]);
+% legend('Origin', '7-Point');
+% 
+% subplot(2,1,2);
+% fx = fftshift(fft(gx));
+% fy = fftshift(fft(gy));
+% f = -(length(gx))/2:(length(gx)/2)-1;
+% plot(f,abs(fx), f, abs(fy));
+% xlim([-inf inf]);
+% legend('Origin', '7-Point');
--- Assignment/paeng/Digital_Signal_Process/DSP_Project_2.m
+++ Assignment/paeng/Digital_Signal_Process/DSP_Project_2.m
@@ -2,25 +2,67 @@ clear all;
 
 %% init
 [x, map] = imread('Lena.png');
+origin = x;
 y = uint8(zeros(size(x)));
+b = 10;
 [xRow, xCol] = size(x);
 
-t= [1:xRow];
-n = length(t);
+n = xRow;
 f = -n/2:(n/2)-1;
-point = 60;
 
-
-%% No Zero Padding
-for ii = (2):(xRow-1)
-    for jj = (2):(xCol-1)
-        y(ii,jj) = 3 * x(ii, jj) - (x(ii + 1, jj) + x(ii - 1, jj) + x(ii, jj + 1) + x(ii, jj - 1)) + (0.25) * (x(ii + 1, jj + 1) + x(ii + 1, jj - 1) + x(ii - 1, jj + 1) + x(ii - 1, jj - 1));
+%% Add Zero Padding
+x = [zeros(1,xCol); x; zeros(1,xCol)];
+x = [zeros(xRow + 2,1), x, zeros(xRow + 2,1)];
+    
+%% main
+for ii = (2):(xRow+1)
+    for jj = (2):(xCol+1)
+        y(ii - 1,jj - 1) = 3 * x(ii, jj) - (x(ii + 1, jj) + x(ii - 1, jj) + x(ii, jj + 1) + x(ii, jj - 1)) + (0.25) * (x(ii + 1, jj + 1) + x(ii + 1, jj - 1) + x(ii - 1, jj + 1) + x(ii - 1, jj - 1));
     end
 end
-y(1,1) = 3 * x(1, 1) - (x(2, 1) + x(1, 2)) + (0.25) * (x(2, 2));
-y(xRow,xCol) = 3 * x(xRow, xCol) - (x(xRow - 1, xCol) + x(xRow, xCol - 1)) + (0.25) * (x(xRow - 1, xCol - 1));
 
+z = origin + b * y;
+
+gx = reshape(origin, 1,[]);  % Graph X
+gz = reshape(z, 1,[]);  % Graph z
 %% file save
-fileName = strcat('LenaOutuput_',string(point),'_point_LPF.jpg');
-imwrite(y, map, fileName);
+fileName = strcat('HPF/LenaOutuput_',string(b),'_HPF.jpg');
+% imwrite(z, map, fileName);
 
+matFileName_gx = strcat(pwd,'\data\', string(point), '_point_gx.mat');
+matFileName_gz = strcat(pwd,'\data\', string(point), '_point_gz.mat');
+save(matFileName_gx, 'gx');
+save(matFileName_gz, 'gz');
+
+%% Origin & 3-Point Graph
+% load(fullfile(pwd, '\data\3_point_gx.mat'));
+% load(fullfile(pwd, '\data\3_point_gz.mat'));
+% subplot(2,1,1);
+% plot(1:length(gx), gx, 1:length(gy), gy);
+% xlim([0 inf]);
+% legend('Origin', '3-Point');
+
+% subplot(2,1,2);
+% fx = fftshift(fft(gx));
+% fy = fftshift(fft(gy));
+% f = -(length(gx))/2:(length(gx)/2)-1;
+% plot(f,abs(fx), f, abs(fy));
+% xlim([-inf inf]);
+% legend('Origin', '3-Point');
+
+%% Origin & 7-Point Graph
+% load(fullfile(pwd, '\data\7_point_gx.mat'));
+% load(fullfile(pwd, '\data\7_point_gy.mat'));
+% 
+% subplot(2,1,1);
+% plot(1:length(gx), gx, 1:length(gy), gy);
+% xlim([0 inf]);
+% legend('Origin', '7-Point');
+% 
+% subplot(2,1,2);
+% fx = fftshift(fft(gx));
+% fy = fftshift(fft(gy));
+% f = -(length(gx))/2:(length(gx)/2)-1;
+% plot(f,abs(fx), f, abs(fy));
+% xlim([-inf inf]);
+% legend('Origin', '7-Point');
